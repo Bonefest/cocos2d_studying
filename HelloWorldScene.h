fd678bfc4ae30ef5e767c16717d13028a4b70152
Bonefest <tereshenko1@ukr.net>
2019-07-14 10:42:54 +0300

    Simple RakNet chat

--- HelloWorldScene.h
+++ HelloWorldScene.h
@@ -28,97 +28,20 @@
 #include "cocos2d.h"
 #include <iostream>
 #include <list>
-#include <RakPeerInterface.h>
-
-const int TRACKS = 3;
-const double SPAWN_TIME = 1.5;
-const double BLOCK_SIZE = 64;
-const double LEVEL_WAIT = 2.0f;
-const double LEVEL_TIME = 10.0f;
-const int COUNTERS = 3;
-
-class Block: public cocos2d::Node {
-public:
-    static Block* createBlock(double speed,cocos2d::Color3B color = cocos2d::Color3B::WHITE);
-
-    void updateBlock(float delta);
-
-    bool isOut() const;
-    bool isClicked(cocos2d::Point point) const;
-
-    double getSpeed() const { return speed; }
-
-    bool isFinished() const { return finished; }
-    bool isHiding() const { return hiding; }
-    void setHiding(bool t) { hiding = t; }
-
-    void onFadeFinish() { finished = true; }
-private:
-    bool finished;
-    bool hiding;
-    double speed;
-
-
-
-};
 
 class HelloWorld : public cocos2d::Scene
 {
 public:
     virtual bool init();
 
-    void draw(cocos2d::Renderer* render,const cocos2d::Mat4& mat,uint32_t t);
-
     static Scene* createScene();
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
 
-    void update(float delta);
-
-    void onClickBegin(cocos2d::Event* event);
-    void onTouchBegin(const std::vector<cocos2d::Touch*>& touches,cocos2d::Event* event);
 
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
-private:
-    RakNet::RakPeerInterface* peer;
 
-    cocos2d::Color4F clearColor;
-    cocos2d::Color4F speedColor;
-
-    int status;
-
-    double currentSpeed;
-
-    double currentSpawnTime;
-    double spawnTime;
-
-    double currentLevelTime;
-    double currentColorTime;
-
-    int level;
-    int score;
-
-    cocos2d::Label* scoreLabel;
-    cocos2d::Label* message;
-
-    std::list<Block*> activatedBlocks;
-
-    void showBigMessage(double time,const std::string& message,bool fade=true);
-
-    void spawnBlock();
-    void removeBlock(Block* blockPtr);
-    void levelChanger();
-    void colorChanger();
-    void gameOver();
-
-    void prepareToPlay();
-
-    bool changeColor;
-    bool spawnNew;
-    bool gameActive;
-
-    cocos2d::DrawNode* drawer;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
--- MenuScene.h
+++ MenuScene.h
@@ -3,8 +3,31 @@
 
 #include "cocos2d.h"
 #include "ui/UIButton.h"
-#include "HelloWorldScene.h"
+#include "ui/UITextField.h"
+#include "RakPeerInterface.h"
+#include <MessageIdentifiers.h>
 
+#include <deque>
+
+const int MAX_CONNECTIONS = 10;
+
+enum USER_MESSAGES {
+    TEXT_MESSAGE=ID_USER_PACKET_ENUM+1
+};
+
+class MessageQueue : public cocos2d::Label {
+public:
+    static MessageQueue* createBoxTTF(const std::string& path,int fontSize,int maxMessages);
+    void addNewMessage(const std::string& message);
+
+
+private:
+    int maxMessages;
+
+    std::deque<std::string> messagesQueue;
+
+    void reorganize();                      //Преобразовывает строку добавляя новый текст в начало и отрезая конец
+};
 
 class MenuScene : cocos2d::Scene {
 public:
@@ -12,14 +35,30 @@ public:
     static Scene* createScene();
 
     CREATE_FUNC(MenuScene);
+    void menuCloseCallback(cocos2d::Ref* psender);
+
+    void update(float delta);
 
 private:
-    cocos2d::Label* scoreLabel;
-    cocos2d::ui::Button* startButton;
+    RakNet::RakPeerInterface*   peer;
+    RakNet::Packet*             packet;
+    RakNet::SystemAddress       serverAddress;
 
-    void menuCloseCallback(cocos2d::Ref* psender);
-    void startGame();
+    bool isServer;
+    bool connected,connecting;
 
+    cocos2d::ui::TextField* addressField;
+    cocos2d::ui::TextField* portField;
+    cocos2d::ui::TextField* messageField;
+    cocos2d::ui::Button*    connectButton;
+    cocos2d::Label*         messageLabel;
+    cocos2d::Label*         ipLabel;
+    cocos2d::Label*         portLabel;
+    MessageQueue*           messageQueue;
+
+    std::string message;
+
+    void startConnection(cocos2d::Ref* pSender,cocos2d::ui::Widget::TouchEventType type);
 };
 
 #endif // MENUSCENE_H_INCLUDED
